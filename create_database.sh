dff08d1e5967f6d4ab8364358a9be93b47facbbd
hg@bitbucket.org <Andrew McMurry@invitae.com>
2014-11-04 10:45:39 -0800

    create_database now defaults to medgen user

--- create_database.sh
+++ create_database.sh
@@ -16,11 +16,7 @@ WORK=$DATASET
 interpolate_file_config "templates/create_database"           > $WORK/create_database.sql
 interpolate_file_config "templates/create_logger.sql"        >> $WORK/create_database.sql 
 interpolate_file_config "templates/create_memory.sql"        >> $WORK/create_database.sql  
-interpolate_file_config "templates/create_user"               > $WORK/create_user.sql
-interpolate_file_config "templates/drop_user"                 > $WORK/drop_user.sql 
 
-echo "MySQL: attempting to create DATASET=$DATASET as YOU=$USER" 
-mysql -u $USER -p             < $WORK/create_database.sql
-
-echo "MySQL: attempting to create DATASET=$DATASET as YOU=$USER" 
-mysql -u $USER -p -D $DATASET < $WORK/create_tables.sql
+echo "MySQL: attempting to create DATASET=$DATASET" 
+mysql -u medgen -pmedgen < $WORK/create_database.sql
+mysql -u medgen -pmedgen -D $DATASET < $WORK/create_tables.sql
