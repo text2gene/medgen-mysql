db64f1758238bd285a5713f61840d2cd24656b5e
hg@bitbucket.org <Andrew McMurry@invitae.com>
2014-11-04 11:08:56 -0800

    DRY: default db.config user/pass is medgen/medgen

--- medgen/views/view_gene_accession.sql
+++ medgen/views/view_gene_accession.sql
@@ -1,9 +1,18 @@
 call log('medgen.view_gene2accession', 'begin');
 -- ###################################################
 
-drop table if exists medgen.view_gene2accession; 
+drop table if exists 
+medgen.view_gene2accession; 
 
-create table         medgen.view_gene2accession
+create table  
+medgen.view_gene2accession
 select * from gene.gene2accession where RNA_acc_ver like 'NM_%'; 
+
+call medgen.create_index('medgen.view_gene2accession','GeneID'); 
+call medgen.create_index('medgen.view_gene2accession','RNA_acc_ver'); 
+call medgen.create_index('medgen.view_gene2accession','GeneID, RNA_acc_ver'); 
+call medgen.create_index('medgen.view_gene2accession','tax_id'); 
+call medgen.create_index('medgen.view_gene2accession','assembly'); 
+ 
 -- ###################################################
 call log('medgen.view_gene2accession', 'end');
