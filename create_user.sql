442e9e06b6e15faad58014548f9af944b6e76be9
hg@bitbucket.org <Andrew McMurry@invitae.com>
2014-11-04 10:45:51 -0800

    fixed privs for medgen user

--- create_user.sql
+++ create_user.sql
@@ -5,4 +5,16 @@ grant execute on *.* to 'medgen'@'%';
 select 'SUPERUSER privs!' as note; 
 select 'SUPERUSER == SUPERHERO or SUPERVILLIAN. Choose wisely!' as note; 
 
-GRANT ALL PRIVILEGES ON *.* TO 'medgen'@'%' WITH GRANT OPTION; 
+grant all privileges on pubmed.*       to 'medgen'@'%' with grant option; 
+grant all privileges on PubTator.*     to 'medgen'@'%' with grant option; 
+grant all privileges on SETH.*         to 'medgen'@'%' with grant option; 
+
+grant all privileges on medgen.*       to 'medgen'@'%' with grant option; 
+grant all privileges on umls.*         to 'medgen'@'%' with grant option; 
+
+grant all privileges on clinvar.*      to 'medgen'@'%' with grant option; 
+grant all privileges on GTR.*          to 'medgen'@'%' with grant option; 
+
+grant all privileges on gene.*         to 'medgen'@'%' with grant option; 
+grant all privileges on hugo.*         to 'medgen'@'%' with grant option; 
+grant all privileges on GeneReviews.*  to 'medgen'@'%' with grant option; 
