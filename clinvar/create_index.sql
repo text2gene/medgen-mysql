276a86bb0b71ee24b22fa58c9ef4d08be42d90ea
hg@bitbucket.org <Andrew McMurry@invitae.com>
2014-11-04 14:50:07 -0800

    refactored: clinvar create index

---
+++ clinvar/create_index.sql
@@ -0,0 +1,30 @@
+call log('create_index.sql', 'begin'); 
+
+call create_index('gene_condition_source_id', 'GeneID');
+call create_index('gene_condition_source_id', 'Symbol');
+call create_index('gene_condition_source_id', 'ConceptID');
+call create_index('gene_condition_source_id', 'DiseaseName');
+call create_index('gene_condition_source_id', 'SourceName');
+call create_index('gene_condition_source_id', 'SourceID');
+call create_index('gene_condition_source_id', 'DiseaseMIM');
+
+call create_index('disease_names','DiseaseName');
+call create_index('disease_names','SourceName');
+call create_index('disease_names','SourceID');
+call create_index('disease_names','DiseaseMIM');
+
+call create_index('gene_specific_summary','GeneID');
+call create_index('gene_specific_summary','Symbol');
+
+call create_index('variant_summary','GeneID');
+call create_index('variant_summary','Symbol');
+call create_index('variant_summary','variant_type');
+call create_index('variant_summary','AlleleID');
+call create_index('variant_summary','rs');
+call create_index('variant_summary','TestedInGTR');
+call create_index('variant_summary','HGVS_c'); 
+call create_index('variant_summary','HGVS_p'); 
+
+call create_index('clingen_gene_curation_list','GeneID'); 
+
+call log('create_index.sql', 'done'); 
