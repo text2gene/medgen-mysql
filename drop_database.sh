e25aed016b389bc1bd8ebe8e596dd799363fa814
hg@bitbucket.org <Andrew McMurry@invitae.com>
2014-11-04 10:45:19 -0800

    added 'make' target for drop database

---
+++ drop_database.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source common.sh 
+
+require   $1 "pick a dataset like ./drop_database clinvar" 
+
+dbconfig="$1/db.config"
+
+require $dbconfig "DATASET/db.config not found" 
+source  $dbconfig
+
+require $DATASET  "which dataset?" 
+
+mysql -u medgen -pmedgen -e "drop database $DATASET" 
