3a799ed6b7cd61b5613bab02224b5cde5d9ca3f9
hg@bitbucket.org <Andrew McMurry@invitae.com>
2014-11-24 20:28:33 -0800

    create user now uses the if-exists workaround

--- create_user.sql
+++ create_user.sql
@@ -1,3 +1,8 @@
+-- Drop user if exists 
+-- http://stackoverflow.com/questions/598190/mysql-check-if-the-user-exists-and-drop-it 
+GRANT USAGE ON  *.* TO 'medgen'@'%';
+DROP USER 'medgen'@'%';
+
 create user 'medgen'@'%' identified by 'medgen';
 grant all on *.* to 'medgen'@'%';
 grant execute on *.* to 'medgen'@'%';
