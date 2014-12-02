58f5cf7356c8597afaa9f47b01fce7358e17b820
hg@bitbucket.org <Andrew McMurry@invitae.com>
2014-12-01 16:27:53 -0800

    BIOMED-248: Fixed Mode Of Inheritance, see freq('view_mode_of_inheritance', 'Inheritance')

--- medgen/views/view_mode_of_inheritance.sql
+++ medgen/views/view_mode_of_inheritance.sql
@@ -3,22 +3,20 @@ call log('view_mode_of_inheritance.sql', 'begin');
 
 drop   table if exists view_mode_of_inheritance; 
 create table           view_mode_of_inheritance
-select DISTINCT 
-       OMIM_SemanticType as SemanticType, 
-       SourceVocab       as SourceVocab,
-       OMIM_ConceptID    as ConceptID,
-       HPO_Name          as Inheritance
-from view_medgen_hpo_omim 
-where relationship = 'inheritance_type_of'; 
+select DISTINCT
+ OMIM_CUI       as OMIM_ConceptID, 
+ MIM_number     as OMIM_ID,
+ OMIM_name      as OMIM_Name, 
+ HPO_name       as Inheritance, 
+ HPO_CUI        as HPO_ConceptID, 
+ HPO_ID         as HPO_ID, 
+ MedGen_name    as Name, 
+ MedGen_source  as SourceVocab, 
+ STY            as SemanticType
+from medgen_hpo_omim
+where Relationship = 'inheritance_type_of'; 
 
--- alter table view_mode_of_inheritance ','ConceptID) ; 
--- alter table view_mode_of_inheritance add column Name varchar(1000) default null; 
-
--- update 
---   view_mode_of_inheritance  as Inherit, 
---   view_concept_preferred    as Term
--- set    Inherit.Name       = Term.Name 
--- where  Inherit.ConceptID  = Term.ConceptID;
+call freq('view_mode_of_inheritance', 'Inheritance');
 
 -- ################################################################
 call log('view_mode_of_inheritance.sql', 'done');
