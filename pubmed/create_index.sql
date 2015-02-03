16fb8ecd09b660c5d0c965505520fd9f9eb6e005
hg@bitbucket.org <Andrew.McMurry@invitae.com>
2015-02-03 13:39:36 -0800

    updated indexes for pubmed central ids

--- pubmed/create_index.sql
+++ pubmed/create_index.sql
@@ -1,18 +1,8 @@
 call log('create_index.sql', 'begin'); 
 
-call log('file_list', 'index'); 
-
-alter table file_list add index (PMCID);
-
-call log('file_list', 'done'); 
---
---
-call log('PMC', 'index'); 
-
-alter table PMC  add index (PMID);
-alter table PMC  add index (PMCID, PMID);
-alter table PMC  add index (PMID, PMCID);
-
-call log('PMC', 'index done'); 
+call create_index('file_list', 'PMCID'); 
+call create_index('deleted',   'PMID'); 
+call create_index('PMC',       'PMID'); 
+call create_index('PMC',       'PMCID'); 
 
 call log('create_index.sql', 'done'); 
