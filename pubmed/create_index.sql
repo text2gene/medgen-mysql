d86525bba540fd84db8aa050764152f33675bfe6
McMurry <Andrew.McMurry@invitae.com>
2015-02-26 14:25:24 -0800

    BIOMED-322: Make DOI a indexable field for PubMed Central

--- pubmed/create_index.sql
+++ pubmed/create_index.sql
@@ -3,6 +3,7 @@ call log('create_index.sql', 'begin');
 call create_index('file_list', 'PMCID'); 
 call create_index('deleted',   'PMID'); 
 call create_index('PMC',       'PMID'); 
-call create_index('PMC',       'PMCID'); 
+call create_index('PMC',       'PMCID');
+call create_index('PMC',       'DOI'); 
 
 call log('create_index.sql', 'done'); 
