a4b9e86759fec12a1b06b6b4c5ff20e900a336e1
Timothy Laurent <timothy.laurent@invitae.com>
2015-05-04 16:05:12 -0700

    added some error detection to parsing function

--- clinvar/clinvar_hgvs.py
+++ clinvar/clinvar_hgvs.py
@@ -1,36 +1,67 @@
 from lxml import etree
-from lxml import objectify
 import os.path
 
 
 def get_hgvs_from_clinvarset( elem ):
+    """
+    Gets HGVS strings from a ClinVarSet element
+    :param elem: ClinVarSet
+    :return: List of HGVS strings
+    """
     hgvs = []
     attributes = elem.xpath("ReferenceClinVarAssertion//Attribute[contains(@Type,'HGVS')]")
     for e in attributes:
         hgvs.append(e.text)
+    if len(hgvs) == 0:
+        hgvs.append('')
     return hgvs
 
 
 def get_variantid_from_clinvarset( elem ):
+    """
+    Gets variant ID from a ClinVarSet Element
+    :param elem: ClinVarSet
+    :return:    Variant ID
+    """
     try:
         # return elem.xpath("ReferenceClinVarAssertion//MeasureSet[@Type='Variant']")[0].attrib['ID']
-        return elem.find('ReferenceClinVarAssertion').find('MeasureSet').attrib['ID']
+        out = elem.find('ReferenceClinVarAssertion').find('MeasureSet').attrib['ID']
+        if out is None:
+            out = ''
+        return out
     except:
         return ''
 
 
 def get_clinvar_accession_from_clinvarset( elem ):
+    """
+    Gets the Clinvar accession from a ClinVarSet element
+    :param elem: ClinVarSet Element
+    :return: ClinVar Accession
+    """
     ## elem.xpath("//ClinVarAccession")[0].attrib['Acc'] ## this only works sometimes
     try:
         # return elem.xpath("//ClinVarAccession")[0].attrib['Acc'] ## this only works sometimes
-        return elem.find("ReferenceClinVarAssertion").find("ClinVarAccession").attrib['Acc']
+        out = elem.find("ReferenceClinVarAssertion").find("ClinVarAccession").attrib['Acc']
+        if out is None:
+            out = ''
+        return out
     except:
         return ''
 
 
 def get_alleleid_from_clinvarset( elem ):
+    """
+    Gets the allele ID from a ClinVarSet element in Clinvar XML
+    :param elem: ClinVarSet xml element
+    :return: AlleleId
+    """
+
     try:
-        return elem.find('ReferenceClinVarAssertion').find('MeasureSet').find('Measure').attrib['ID']
+        out = elem.find('ReferenceClinVarAssertion').find('MeasureSet').find('Measure').attrib['ID']
+        if out is None:
+            out = ''
+        return out
     except:
         return ''
 
