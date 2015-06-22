8afd59a5e85e65c5bb9ba7414733e9285fc47ab3
McMurry <Andrew.McMurry@invitae.com>
2015-06-22 14:58:01 -0700

    medgen relationships indexes"

--- medgen/create_index.sql
+++ medgen/create_index.sql
@@ -10,6 +10,11 @@ call create_index('medgen_pubmed','UID');  -- MedGen primary key
 call create_index('medgen_pubmed','CUI');  -- UMLS   primary key
 call create_index('medgen_pubmed','PMID'); -- PubMed primary key
 
+-- MedGen RELATIONSHIPS
+call create_index('MGREL','CUI1'); -- Concept 1
+call create_index('MGREL','CUI2'); -- Concept 2
+call create_index('MGREL','REL');  -- Relationship
+call create_index('MGREL','RELA'); -- Relationsihp Additional 
 
 -- OMIM References (directly from OMIM to PubMed)
 call create_index('omim_pubmed','MIM');  -- MIM
