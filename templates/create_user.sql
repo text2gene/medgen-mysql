583791f3b9fe5e28c175cd42c3f6a592e297a9e7
McMurry <Andrew.McMurry@invitae.com>
2015-07-26 19:05:06 -0700

    minor doc clean

--- templates/create_user.sql
+++ templates/create_user.sql
@@ -1,9 +1,3 @@
--- TODO: DRY: FabFile.py 
--- 
--- Create user 
-
 create user '@@@DB_USER'@'localhost' identified by '@@@DB_PASS';
 grant all on @@@DATASET.* to '@@@DB_USER'@'localhost';
 grant execute on @@@DATASET.* to '@@@DB_USER'@'localhost';
-
--- end create user 
