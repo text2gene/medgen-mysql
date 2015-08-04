750e08a0fc86d62122481f3c3bff10c384aa7dd1
McMurry <Andrew.McMurry@invitae.com>
2015-08-03 17:45:13 -0700

    defaults to medgen at localhost

--- create_user.sql
+++ create_user.sql
@@ -1,7 +1,7 @@
 -- Drop user if exists 
 -- http://stackoverflow.com/questions/598190/mysql-check-if-the-user-exists-and-drop-it 
-GRANT USAGE ON  *.* TO 'medgen'@'%';
-DROP USER 'medgen'@'%';
+GRANT USAGE ON  *.* TO 'medgen'@'localhost';
+DROP USER 'medgen'@'localhost';
 
 -- MySQL anonymous user can really screw things up.
 -- http://bugs.mysql.com/bug.php?id=31061
@@ -9,27 +9,27 @@ DROP USER 'medgen'@'%';
 GRANT USAGE ON  mysql.* TO ''@'localhost';
 DROP USER ''@'localhost';
 
-create user 'medgen'@'%' identified by 'medgen';
+create user 'medgen'@'localhost' identified by 'medgen';
 
-grant all on *.* to 'medgen'@'%';
-grant execute on *.* to 'medgen'@'%';
+grant all on *.* to 'medgen'@'localhost';
+grant execute on *.* to 'medgen'@'localhost';
 
 -- MedGen directly references to these sources
-grant all privileges on clinvar.*      to 'medgen'@'%' with grant option; 
-grant all privileges on gene.*         to 'medgen'@'%' with grant option; 
-grant all privileges on GeneReviews.*  to 'medgen'@'%' with grant option; 
-grant all privileges on GO.*           to 'medgen'@'%' with grant option; 
-grant all privileges on GTR.*          to 'medgen'@'%' with grant option; 
-grant all privileges on hpo.*          to 'medgen'@'%' with grant option; 
-grant all privileges on hugo.*         to 'medgen'@'%' with grant option; 
-grant all privileges on medgen.*       to 'medgen'@'%' with grant option; 
-grant all privileges on orphanet.*     to 'medgen'@'%' with grant option; 
-grant all privileges on pubmed.*       to 'medgen'@'%' with grant option; 
-grant all privileges on umls.*         to 'medgen'@'%' with grant option; 
+grant all privileges on clinvar.*      to 'medgen'@'localhost' with grant option; 
+grant all privileges on gene.*         to 'medgen'@'localhost' with grant option; 
+grant all privileges on GeneReviews.*  to 'medgen'@'localhost' with grant option; 
+grant all privileges on GO.*           to 'medgen'@'localhost' with grant option; 
+grant all privileges on GTR.*          to 'medgen'@'localhost' with grant option; 
+grant all privileges on hpo.*          to 'medgen'@'localhost' with grant option; 
+grant all privileges on hugo.*         to 'medgen'@'localhost' with grant option; 
+grant all privileges on medgen.*       to 'medgen'@'localhost' with grant option; 
+grant all privileges on orphanet.*     to 'medgen'@'localhost' with grant option; 
+grant all privileges on pubmed.*       to 'medgen'@'localhost' with grant option; 
+grant all privileges on umls.*         to 'medgen'@'localhost' with grant option; 
 
 -- MedGen is able to link to Human Genetic Variation Society (HGVS) formatted datasources
 -- http://www.hgvs.org/mutnomen
-grant all privileges on v2p.*             to 'medgen'@'%' with grant option;
-grant all privileges on PubTator.*        to 'medgen'@'%' with grant option; 
-grant all privileges on SETH.*            to 'medgen'@'%' with grant option; 
-grant all privileges on PersonalGenomes.* to 'medgen'@'%' with grant option; 
+grant all privileges on v2p.*             to 'medgen'@'localhost' with grant option;
+grant all privileges on PubTator.*        to 'medgen'@'localhost' with grant option; 
+grant all privileges on SETH.*            to 'medgen'@'localhost' with grant option; 
+grant all privileges on PersonalGenomes.* to 'medgen'@'localhost' with grant option; 
