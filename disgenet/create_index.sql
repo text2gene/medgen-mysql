a7a27bcadbdf624cc414abee23fa2722bef61a00
nthmost <naomi@nthmost.com>
2019-07-29 23:36:26 +0000

    DisGeNET updates

--- disgenet/create_index.sql
+++ disgenet/create_index.sql
@@ -5,3 +5,4 @@ update disease2gene set ConceptID = replace(ConceptID, 'umls:', '');
 
 call create_index('disease2gene', 'ConceptID'); 
 call create_index('disease2gene', 'ConceptName');
+call create_index('disease2gene', 'GeneID,ConceptID');
--- disgenet/create_tables.sql
+++ disgenet/create_tables.sql
@@ -14,15 +14,16 @@ drop table if exists disease2gene;
 create table         disease2gene(
        GeneID        int(10) unsigned,
        Symbol        varchar(25),
-       GeneText      text,
-       ConceptID     varchar(15),
+       ConceptID     varchar(25),
        ConceptName   varchar(255),
        Score         float(5,4),
        PMID_count    int,
-       AssocType     text, 
-       SourceVocab   text
+       SNP_count     int,
+       SourceVocab   varchar(255)
 );
+
 
 call utf8_unicode('disease2gene'); 
 
 call log('create_tables.sql', 'done');
+
